da449612f6ee4685c62a7bf8e68c738c2391b214
Arcfundinfo <94897257+Arcfundinfo@users.noreply.github.com>
2022-06-13 09:42:02 +0800

    create

---
+++ contracts/ETHRejecter.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.6.8;
+
+interface IAuctionModified {
+    function createBid(uint256 tokenId, uint256 amount) external payable;
+}
+
+contract ETHRejecter {
+    function relayBid(
+        address auction,
+        uint256 tokenId,
+        uint256 amount
+    ) external payable {
+        IAuctionModified(auction).createBid{value: amount}(tokenId, amount);
+    }
+
+    receive() external payable {
+        assert(1 == 2);
+    }
+}
