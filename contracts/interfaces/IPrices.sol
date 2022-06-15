b0e3d7667dc071d81bcce99d7cf121d9424c966b
Arcfundinfo <94897257+Arcfundinfo@users.noreply.github.com>
2022-06-15 17:40:56 +0800

    Update IPrices.sol

--- contracts/interfaces/IPrices.sol
+++ contracts/interfaces/IPrices.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.8;
 
+import './IPriceFeed.sol';
 
 interface IPrices {
-  event AddFeed(uint256 indexed currency, uint256 indexed base);
+  event AddFeed(uint256 indexed currency, uint256 indexed base, IPriceFeed feed);
 
-  function feedFor(uint256 _currency, uint256 _base) external view returns (address);
+  function feedFor(uint256 _currency, uint256 _base) external view returns (IPriceFeed);
 
   function priceFor(
     uint256 _currency,
